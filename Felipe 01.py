03c497b8547030472c783be62c59a3cb79406ab0
Felipe Miguel <felipemiguelcaetano45@gmail.com>
2025-03-13 20:24:18 -0300

    Felipe 01.py

--- Felipe 01.py
+++ Felipe 01.py
@@ -1,4 +1,5 @@
 # LOGICA-NIVEL-1
+#2. Etiqueta - Elabore um programa que imprima seu nome na primeira linha, o estado em que você mora na segunda, e seu município na terceira.
 
 print("Felipe Miguel Caetano De Oliveira")
 print("Rio De Janeiro")
